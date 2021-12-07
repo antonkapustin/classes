161c58d9593e07f13de5028b9f24691869fa49ac
AntonKapustin <antonkapustsin@gmail.com>
2021-12-07 17:26:23 +0300

    update ShowOptions method

--- simple-table.js
+++ simple-table.js
@@ -8,6 +8,13 @@ export class SimpleTable {
     this.options = options;
     this.headerTemplate = '<p class="grid__element" >{{label}}</p>';
     this.bodyTemplate = '<p class="grid__element">{{{{key}}}}</p>';
+    this.inpTemplate = `<li class="option__item">
+        <label class="options__label">
+        <input class="options__input" type="checkbox" value="{{key}}" {{checked}}>
+        <span class="options__text">{{key}}</span>
+        </label>
+        </li>`;
+    this.showedOptions = [...options.columns];
     this.initialeData = [...data];
 
     setTimeout(() => {
@@ -16,18 +23,31 @@ export class SimpleTable {
     });
     this.emitter = new EventEmiter();
   }
-  render(data) {
+  render(data = this.data) {
+    this.options.columns = this.options.columns.filter((element) => {
+      return element.checked.indexOf("checked") === 0;
+    });
     const grid = document.createElement("div");
+    const list = document.createElement("ul");
 
     let headerStr = this.renderHeader();
     let bodyStr = this.renderBody(data);
+    const inputs = this.renderInp(this.showedOptions);
 
     grid.classList.add("grid");
+    list.classList.add("options__list");
 
     grid.innerHTML = headerStr + bodyStr;
+    list.innerHTML = inputs;
 
     this.hostElement.innerHTML = "";
+    this.hostElement.append(list);
     this.hostElement.append(grid);
+
+    let gridElements = this.hostElement.querySelectorAll("[data-dom='grid']");
+    gridElements.forEach((el) => {
+      el.style.gridTemplateColumns = `25px repeat(${this.options.columns.length}, 1fr)`;
+    });
   }
 
   renderHeader() {
@@ -36,6 +56,9 @@ export class SimpleTable {
     });
 
     return `<div class="grid__header" data-dom="grid">
+    <label class="grid__element_label">
+        <input type="checkbox" class="element_input">
+    </label>
                     ${template.join("")}
                  </div>`;
   }
@@ -45,7 +68,7 @@ export class SimpleTable {
       return renderToDom(el, this.bodyTemplate);
     });
 
-    template = template.join("");
+    template = `<p class="grid__element"></p>` + template.join("");
 
     let array = data.map((el) => {
       return renderToDom(el, template);
@@ -55,8 +78,16 @@ export class SimpleTable {
                     ${array.join("")}
                 </div>`;
   }
+  renderInp(data) {
+    let template = data.map((el) => {
+      return renderToDom(el, this.inpTemplate);
+    });
+    return template.join("");
+  }
 
-  applyHandler() {}
+  applyHandler() {
+    this.hostElement.addEventListener("click", this.onShowOptions.bind(this));
+  }
 
   filter(element) {
     this.data = this.initialeData.filter((el) => {
@@ -65,5 +96,35 @@ export class SimpleTable {
       );
     });
     this.render();
+  }
+
+  onShowOptions(event) {
+    let current = event.target;
+    while (current !== this.hostElement) {
+      if (current.classList.contains("options__input")) {
+        break;
+      }
+      current = current.parentElement;
+    }
+    if (current === this.hostElement) {
+      return;
+    }
+    this.showOptions(current.value);
+    this.render();
+  }
+
+  showOptions(value) {
+    this.options.columns = this.showedOptions.map((el) => {
+      if (el.key === value) {
+        if (el.checked === "checked") {
+          el.checked = "";
+        } else {
+          el.checked = "checked";
+        }
+        return el;
+      } else {
+        return el;
+      }
+    });
   }
 }
--- sortable-table.js
+++ sortable-table.js
@@ -1,75 +1,74 @@
 import { SimpleTable } from "./simple-table.js";
 
 export class SortableTable extends SimpleTable {
-    constructor(data, hostElement, options){
-        super(data, hostElement, options)
-        this.initialData = [...this.data]
-        this.sort();
+  constructor(data, hostElement, options) {
+    super(data, hostElement, options);
+    this.initialData = [...this.data];
+    this.sort();
+  }
+  render() {
+    this.headerTemplate =
+      '<button class="grid__element grid__element_button" type="button" value="{{key}}">{{label}}</button>';
+    super.render();
+  }
+  applyHandler() {
+    super.applyHandler();
+    this.hostElement.addEventListener("click", this.onSort.bind(this));
+  }
+
+  onSort(event) {
+    let current = event.target;
+
+    while (current !== this.hostElement) {
+      if (current.classList.contains("grid__element_button")) {
+        break;
+      }
+      current = current.parentElement;
     }
-    render(){
-        this.headerTemplate = '<button class="grid__element grid__element_button" type="button" value="{{key}}">{{label}}</button>';
-        super.render()
-    }
-    applyHandler(){
-        this.hostElement.addEventListener("click",this.onSort.bind(this));
+    if (current === this.hostElement) {
+      return;
     }
 
-    onSort(event){
-        let current = event.target;
+    let key = current.value;
 
-        while (current !== this.hostElement) {
-            if (current.classList.contains("grid__element_button")) {
-              break;
-            }
-            current = current.parentElement;
-          }
-          if (current === this.hostElement) {
-            return;
-          }
-
-          let key = current.value;
-
-
-          if (key === this.options.sortable.key) {
-            if (this.options.sortable.value === "ASC") {
-              this.options.sortable.value = "DESC";
-            } else if (this.options.sortable.value === "DESC") {
-              this.options.sortable.value = null;
-            } else if (this.options.sortable.value === null) {
-              this.options.sortable.value = "ASC";
-            }
-          } else {
-            this.options.sortable.key = key;
-            this.options.sortable.value = "ASC";
-          }
-
-          this.sort()
-          this.hostElement.removeEventListener("click", this.onSort)
-          this.render()
+    if (key === this.options.sortable.key) {
+      if (this.options.sortable.value === "ASC") {
+        this.options.sortable.value = "DESC";
+      } else if (this.options.sortable.value === "DESC") {
+        this.options.sortable.value = null;
+      } else if (this.options.sortable.value === null) {
+        this.options.sortable.value = "ASC";
+      }
+    } else {
+      this.options.sortable.key = key;
+      this.options.sortable.value = "ASC";
     }
-    sort(){
-        let data = [...this.initialData].sort((a,b)=>{
-            let valueA = a[this.options.sortable.key];
-            let valueB = b[this.options.sortable.key];
 
-            if(valueA > valueB){
-                return 1
-            }else if(valueA < valueB){
-                return -1
-            } else{
-                return 0
-            }
-        })
-        if (this.options.sortable.value === "ASC") {
-            return this.data = data
-          }
-          if (this.options.sortable.value === "DESC") {
-            return this.data = data.reverse();
-              
-          }
-          if (this.options.sortable.value === null) {
-            return this.data = [...this.initialData];
-          }
+    this.sort();
+    this.hostElement.removeEventListener("click", this.onSort);
+    this.render();
+  }
+  sort() {
+    let data = [...this.initialData].sort((a, b) => {
+      let valueA = a[this.options.sortable.key];
+      let valueB = b[this.options.sortable.key];
 
+      if (valueA > valueB) {
+        return 1;
+      } else if (valueA < valueB) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    if (this.options.sortable.value === "ASC") {
+      return (this.data = data);
     }
+    if (this.options.sortable.value === "DESC") {
+      return (this.data = data.reverse());
+    }
+    if (this.options.sortable.value === null) {
+      return (this.data = [...this.initialData]);
+    }
+  }
 }
